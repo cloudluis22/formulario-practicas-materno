1ec91a7849f530a6455743c9f231e996c1912217
cloudluis22 <cloudluis22@hotmail.com>
2022-04-07 18:41:25 -0500

    Finalizado comportamiento inicial de pagina principal

--- src/views/App.js
+++ src/views/App.js
@@ -6,6 +6,7 @@ import {
   Navigate,
 } from 'react-router-dom';
 import { Formulario } from './Formulario';
+import { MainForm } from './MainForm';
 import { Pacientes } from './Pacientes';
 
 export const App = () => {
@@ -13,8 +14,8 @@ export const App = () => {
     <Router>
       <Routes>
         <Route path='/pacientes' element={<Pacientes />} />
-        <Route path='/registro/:id' element={<Formulario />} />
-        <Route path='/registro' element={<Formulario />} />
+        <Route path='/edicion/:id' element={<Formulario />} />
+        <Route path='/registro' element={<MainForm />} />
         <Route path='*' element={<Navigate to='/pacientes' replace />} />
       </Routes>
     </Router>
--- src/views/Pacientes.js
+++ src/views/Pacientes.js
@@ -1,23 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const Pacientes = () => {
+  const [Data, setData] = useState({
+    loading: true,
+    data: [],
+    ok: false,
+  });
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/v1/obtener-pacientes')
+      .then((response) => {
+        console.log(response.data.pacientes);
+        setData({
+          loading: false,
+          data: response.data.pacientes,
+          ok: true,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setData({
+          loading: false,
+          data: [],
+          ok: false,
+        });
+      });
+  }, []);
+
   return (
     <div className='bcg-main-color'>
       <h1 className='text-white text-center mt-3'>
         Información de los Pacientes
       </h1>
-      <div
-        className='d-flex justify-content-center align-items-center flex-column mt-5'
-        style={{
-          width: '100%',
-        }}>
+      {Data.loading && (
         <div
-          className='spinner-border text-primary text-light'
-          role='status'
-          style={{ width: '6rem', height: '6rem' }}
-        />
-        <p className='text-white mt-3'> Cargando Información... </p>
-      </div>
+          className='d-flex justify-content-center align-items-center flex-column mt-5'
+          style={{
+            width: '100%',
+          }}>
+          <div
+            className='spinner-border text-primary text-light'
+            role='status'
+            style={{ width: '6rem', height: '6rem' }}
+          />
+          <p className='text-white mt-3'> Cargando Información... </p>
+        </div>
+      )}
+      {!Data.loading && !Data.ok && (
+        <div>
+          <p> Hubo un error al traer la información </p>
+        </div>
+      )}
+
+      {!Data.loading && Data.ok && (
+        <table className='table table-primary table-striped'>
+          <thead>
+            <tr>
+              <th className='col'> ID del Paciente </th>
+              <th className='col'> Nombre del Paciente </th>
+              <th className='col'> Género </th>
+              <th className='col'> Número Telefónico </th>
+              <th className='col'> Acciones </th>
+            </tr>
+          </thead>
+          <tbody>
+            {Data.data.map((paciente) => {
+              return (
+                <tr>
+                  <td> {paciente.IdPaciente} </td>
+                  <td> {paciente.NombrePaciente} </td>
+                  <td> {paciente.Genero} </td>
+                  <td> {paciente.Celular} </td>
+                  <td>
+                    <button className='btn btn-success'> Ver Paciente </button>
+                    <button className='btn btn-danger'>
+                      Eliminar Paciente
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
